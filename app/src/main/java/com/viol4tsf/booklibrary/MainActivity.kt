8b38e953feead12fbd5570cd66b8826bd31c2c14
ISergienkoSF <sergienko.ivan2020@mail.ru>
2022-08-17 12:55:00 +0300

    Initialize commit

---
+++ app/src/main/java/com/viol4tsf/booklibrary/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.viol4tsf.booklibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
